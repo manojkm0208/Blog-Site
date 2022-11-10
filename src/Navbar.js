02d84e3cc50b3166d5907bdff3c761ed5b96e46a
Manoj KM <manojkm@Manojs-MacBook-Pro.local>
2022-11-11 00:35:31 +0530

    second commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <h1>Blog-Site</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
